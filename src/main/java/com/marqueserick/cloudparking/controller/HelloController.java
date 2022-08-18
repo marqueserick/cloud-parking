b5615ec140a08be1e4e03b7cfecbcd88aed2decf
marqueserick <erickmarques43@gmail.com>
2022-08-18 18:03:39 -0300

    adiciona swagger

--- src/main/java/com/marqueserick/cloudparking/controller/HelloController.java
+++ src/main/java/com/marqueserick/cloudparking/controller/HelloController.java
@@ -1,11 +1,16 @@
 package com.marqueserick.cloudparking.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Api(tags = "Hello World")
+@RequestMapping("/")
 public class HelloController {
 
     @GetMapping
+    @ApiOperation("Saying hello to the world")
     public String hello() {
         return "Olá Mundo!";
     }
