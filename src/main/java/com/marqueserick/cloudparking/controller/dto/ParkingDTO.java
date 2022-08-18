6eceaa7e03e2d0e8bed1de95ff58d8a7d16e64a7
marqueserick <erickmarques43@gmail.com>
2022-08-18 14:23:35 -0300

    altera exibição da data

--- src/main/java/com/marqueserick/cloudparking/controller/dto/ParkingDTO.java
+++ src/main/java/com/marqueserick/cloudparking/controller/dto/ParkingDTO.java
@@ -1,5 +1,6 @@
 package com.marqueserick.cloudparking.controller.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import java.time.LocalDateTime;
@@ -12,7 +13,9 @@ public class ParkingDTO {
     private String state;
     private String model;
     private String color;
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
     private LocalDateTime entry;
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
     private LocalDateTime exit;
     private Double total;
 
