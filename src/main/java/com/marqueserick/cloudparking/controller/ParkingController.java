6c8ff1e4bf482b98be6721623f388c235a6b1f07
marqueserick <erickmarques43@gmail.com>
2022-08-19 10:42:41 -0300

    remove imports sem uso

--- src/main/java/com/marqueserick/cloudparking/controller/ParkingController.java
+++ src/main/java/com/marqueserick/cloudparking/controller/ParkingController.java
@@ -6,7 +6,6 @@ import com.marqueserick.cloudparking.controller.mapper.ParkingMapper;
 import com.marqueserick.cloudparking.model.Parking;
 import com.marqueserick.cloudparking.service.ParkingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
