1748d711806aae3b584f210214237fec1d12af1a
marqueserick <erickmarques43@gmail.com>
2022-08-22 17:21:32 -0300

    adiciona testContainer

--- src/test/java/com/marqueserick/cloudparking/controller/ParkingControllerTest.java
+++ src/test/java/com/marqueserick/cloudparking/controller/ParkingControllerTest.java
@@ -11,7 +11,7 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import static org.hamcrest.Matchers.equalTo;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class ParkingControllerTest {
+class ParkingControllerTest extends AbstractContainerBase{
 
     @LocalServerPort
     private int port;
